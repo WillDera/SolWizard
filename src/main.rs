9bdda4a60132e38a540a11e6ecf1315d834388a1
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-12-30 21:16:17 +0100

    CleanUp: Clean up main.rs to include only clap args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-// (Full example with detailed comments in examples/01a_quick_example.rs)
-//
-// This example demonstrates clap's "builder pattern" method of creating arguments
-// which the most flexible, but also most verbose.
 extern crate clap;
 use clap::{App, Arg};
-use std::process::Command;
-mod helpers;
+mod contracts;
 
 //TODO: Read/Write to files in contract folder
 //TODO: Add custom code to contract files
@@ -14,55 +9,6 @@ mod helpers;
 fn check_for_node() {
     println!("Hello from node");
 }
-
-// fn install_dependencies() -> std::io::Result<()> {
-//     let dependencies = File::open("../src/install.txt").unwrap();
-//     let reader = BufReader::new(dependencies);
-
-//     for line in reader.lines() {
-//         let line = line.expect("Failed to read line.");
-
-//         Command::new(NPM)
-//             .args(["install", "--save-dev"])
-//             .arg(line)
-//             .status()
-//             .expect("An error occured while installing dependency: {line}");
-//     }
-
-//     Ok(())
-// }
-
-fn erc20(contract: &str, project: &str, filename: &str) {
-    // let path = env::current_dir();
-
-    helpers::mkdir_cd(project).unwrap();
-
-    Command::new("npx.cmd")
-        .arg("hardhat")
-        .status()
-        .expect("node failed to fetch version");
-
-    helpers::install_dependencies().unwrap();
-
-    // change_dir_and_make_file()
-
-    // println!(
-    //     "From the erc20 function: {:?}, {}, {:?}",
-    //     path,
-    //     contract,
-    //     dependencies.as_ref()
-    // );
-}
-
-fn erc721() {
-    println!("From the erc721 function");
-}
-
-fn custom() {
-    println!("From the custom domain...");
-}
-
-//? Try args
 
 fn main() {
     let matches = App::new("Smart Contract Bootstrapper")
@@ -102,15 +48,15 @@ fn main() {
     println!("{} :Hello there", matches.value_of("type").unwrap());
 
     // match
-    let name = matches.value_of("type").unwrap().to_lowercase();
+    let contract_type = matches.value_of("type").unwrap().to_lowercase();
     let project_name = matches.value_of("project_name").unwrap();
     let filename = matches.value_of("filename").unwrap();
 
     match matches.value_of("type").unwrap() {
         "node" => check_for_node(),
-        "erc20" => erc20(&name, project_name, filename),
-        "erc721" => erc721(),
-        "Custom" => custom(),
+        "erc20" => contracts::erc20(&contract_type, project_name, filename),
+        "erc721" => contracts::erc721(),
+        "Custom" => contracts::custom(),
         _ => println!("Don't be crazy!"),
     }
 }
