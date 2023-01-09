9ccf52f3478262cfe13c37a7afdf6a2af673fa2b
Ezeoke Godswill <godswillezeoke@gmail.com>
2023-01-09 22:50:00 +0100

    mod: normal() generates the mundane erc contracts via the redefined erc templates

--- src/create/mod.rs
+++ src/create/mod.rs
@@ -3,14 +3,19 @@ use std::process::Command;
 mod helpers;
 
 /**
- * Triggers contract creation
+ * Triggers Normal contract creation
  * @param contract_type: contract type to be created i.e ERC20 (might be redundant)
  * @param project_name: the project name i.e. DeraDAO
  * @param filename: the contract file name i.e. DeraDAO.sol
  * @param openzeppelin: if openzeppelin imports should be included
  * */
 
-pub fn contract(contract_type: &str, project_name: &str, filename: &str, openzeppelin: bool) {
+pub fn normal(
+    contract_types: Vec<&str>,
+    project_name: &str,
+    filenames: Vec<&str>,
+    openzeppelin: bool,
+) {
     if !Path::new("hardhat.config.json").exists() {
         helpers::mkdir_cd(project_name).unwrap();
 
@@ -19,16 +24,33 @@ pub fn contract(contract_type: &str, project_name: &str, filename: &str, openzep
             .expect("node failed to fetch version");
     }
 
-    // helpers::install_dependencies().unwrap();
+    helpers::install_dependencies().unwrap();
 
-    // helpers::change_dir_and_make_file(filename, openzeppelin, contract_type).unwrap();
+    helpers::change_dir_and_make_file(filenames, openzeppelin, contract_types).unwrap();
 }
 
+/**
+ * Triggers Custom contract creation
+ * @param contract_type: contract type to be created i.e ERC20 (might be redundant)
+ * @param project_name: the project name i.e. DeraDAO
+ * @param filename: the contract file name i.e. DeraDAO.sol
+ * @param openzeppelin: if openzeppelin imports should be included
+ * */
 pub fn contracts(
     contract_types: Vec<&str>,
     project_name: &str,
     filenames: Vec<&str>,
     openzeppelin: bool,
 ) {
-    println!("{:?}", filenames);
+    if !Path::new("hardhat.config.json").exists() {
+        helpers::mkdir_cd(project_name).unwrap();
+
+        Command::new("hardhat.cmd")
+            .status()
+            .expect("node failed to fetch version");
+    }
+
+    helpers::install_dependencies().unwrap();
+
+    helpers::change_dir_and_make_file(filenames, openzeppelin, contract_types).unwrap();
 }
