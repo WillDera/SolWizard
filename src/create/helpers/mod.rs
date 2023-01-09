f4cdcc2780a52a49d5b7d388202f9a6e1db01294
Ezeoke Godswill <godswillezeoke@gmail.com>
2023-01-09 22:51:15 +0100

    fix: use stdout/err to remove yarn logs, added a progress bar (animation is currently rigid)

--- src/create/helpers/mod.rs
+++ src/create/helpers/mod.rs
@@ -1,8 +1,8 @@
-use indicatif::ProgressBar;
+use indicatif::{ProgressBar, ProgressDrawTarget, ProgressStyle};
 use std::fs::{copy, File};
 use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::{env, fs};
 mod template;
 
@@ -22,16 +22,23 @@ pub const YARN: &'static str = "yarn";
 * @param contract_type: type of contract being created
 **/
 pub fn change_dir_and_make_file(
-    filename: &str,
+    filenames: Vec<&str>,
     openzeppelin: bool,
-    contract_type: &str,
+    contract_types: Vec<&str>,
 ) -> std::io::Result<()> {
     env::set_current_dir("contracts").unwrap();
 
-    let snippet = template::generate_snippet(openzeppelin, contract_type);
+    for x in 0..filenames.len() {
+        let snippet = if contract_types.len() == 1 {
+            template::generate_snippet(openzeppelin, contract_types[0])
+        } else {
+            template::generate_snippet(openzeppelin, contract_types[x])
+        };
 
-    let mut file = File::create(filename).unwrap();
-    file.write_all(snippet.as_bytes()).unwrap();
+        let mut file = File::create(filenames[x]).unwrap();
+        file.write_all(snippet.as_bytes()).unwrap();
+    }
+
     Ok(())
 }
 
@@ -42,28 +49,34 @@ pub fn change_dir_and_make_file(
 **/
 pub fn mkdir_cd(project: &str) -> std::io::Result<()> {
     let dest_path = Path::new(project).display().to_string();
-    fs::create_dir_all(project)?;
+    fs::create_dir_all(&dest_path)?;
 
     copy(
-        "./src/contracts/helpers/install.txt",
+        "./src/create/helpers/install.txt",
         &format!("./{}/install.txt", &dest_path),
     )?;
-    env::set_current_dir(&project).unwrap();
 
-    Command::new(YARN)
-        .arg("init")
-        .arg("-y")
-        .status()
-        .expect("Couldnt initialize yarn project");
+    match env::set_current_dir(&dest_path) {
+        Ok(()) => {
+            Command::new(YARN)
+                .arg("init")
+                .arg("-y")
+                .status()
+                .expect("Couldnt initialize yarn project");
 
-    Command::new(YARN)
-        .arg("add")
-        .arg("-D")
-        .arg("hardhat")
-        .arg("--silent")
-        .arg("--no-progress")
-        .status()
-        .expect("An error occured while installing dependency: hardhat");
+            Command::new(YARN)
+                .arg("add")
+                .arg("-D")
+                .arg("hardhat")
+                .stdout(Stdio::null())
+                .stderr(Stdio::null())
+                .output()?;
+        }
+        Err(e) => {
+            println!("Error changing current directory: {}", e);
+            return Err(e);
+        }
+    }
 
     Ok(())
 }
@@ -75,24 +88,35 @@ pub fn install_dependencies() -> std::io::Result<()> {
     let dependencies = File::open("./install.txt").unwrap();
     let reader = BufReader::new(dependencies);
 
-    let bar = ProgressBar::new(5);
+    let pb = ProgressBar::new(5);
+
+    pb.set_style(
+        ProgressStyle::default_bar()
+            .progress_chars("##--")
+            .template(
+                "{spinner:.green} [{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+            )
+            .expect("Invalid progress bar template"),
+    );
+
+    let mut static_line = Box::new(String::new());
 
     for line in reader.lines() {
+        pb.inc(1);
         let line = line.expect("Failed to read line.");
+        *static_line = line.to_string();
+        let message = format!("Installing {}", static_line);
+        pb.set_message(message);
 
         Command::new(YARN)
             .arg("add")
             .arg("-D")
             .arg(line)
-            .arg("--silent")
-            .arg("--no-progress")
-            .arg("--non-interactive")
-            .status()
-            .expect("An error occured while installing dependency: {line}");
-
-        bar.inc(1);
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .output()?;
     }
-    bar.finish();
+    pb.finish_with_message("Dependencies installed");
 
     Ok(())
 }
